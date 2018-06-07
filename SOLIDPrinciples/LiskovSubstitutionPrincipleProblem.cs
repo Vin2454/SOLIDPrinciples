6de74b66080eabcc60157f1693ae893694efa9c9
Vinay Kumar Gandra <v-vinga@microsoft.com>
2018-06-07 14:32:14 +0530

    LiskovSubstitutionPrincipleProblem & LiskovSubstitutionPrincipleSolution files added.

---
+++ SOLIDPrinciples/LiskovSubstitutionPrincipleProblem.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SOLIDPrinciples
+//{
+//    class LiskovSubstitutionPrincipleProblem
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<Customer> Customers = new List<Customer>();
+//            Customers.Add(new SilverCustomer());
+//            Customers.Add(new Enquiry());
+
+//            foreach (Customer o in Customers)
+//            {
+//                o.Add();
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//    class Customer
+//    {
+//        public virtual void Add()
+//        {
+
+//        }
+//        public virtual double getDiscount(double TotalSales)
+//        {
+//            return TotalSales;
+//        }
+//    }
+//    class SilverCustomer : Customer
+//    {
+//        public override double getDiscount(double TotalSales)
+//        {
+//            return base.getDiscount(TotalSales) - 50;
+//        }
+//        public override void Add()
+//        {
+//            Console.WriteLine("Silver Customer");
+//        }
+       
+//    }
+//    class Enquiry : Customer
+//    {
+//        public override double getDiscount(double TotalSales)
+//        {
+//            return base.getDiscount(TotalSales) - 90;
+//        }
+
+//        public override void Add()
+//        {
+//            throw new Exception("Not allowed");
+//        }
+//    }
+//}
---
+++ SOLIDPrinciples/LiskovSubstitutionPrincipleSolution.cs
@@ -0,0 +1,66 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SOLIDPrinciples
+//{
+//    class LiskovSubstitutionPrincipleSolution
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<Customer> Customers = new List<Customer>();
+//            Customers.Add(new SilverCustomer());
+//            Customers.Add(new Enquiry());//error is at compile time only rather than at runtime.
+
+//            foreach (Customer o in Customers)
+//            {
+//                o.Add();
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//    class Customer: IDiscount, IDatabase
+//    {
+//        public virtual void Add()
+//        {
+
+//        }
+//        public virtual double getDiscount(double TotalSales)
+//        {
+//            return TotalSales;
+//        }
+//    }
+//    class SilverCustomer : Customer
+//    {
+//        public override double getDiscount(double TotalSales)
+//        {
+//            return base.getDiscount(TotalSales) - 50;
+//        }
+//        public override void Add()
+//        {
+//            Console.WriteLine("Silver Customer");
+//        }
+
+//    }
+//    class Enquiry : IDiscount
+//    {
+//        public double getDiscount(double TotalSales)
+//        {
+//            return TotalSales - 90;
+//        }
+   
+//    }
+
+//    interface IDiscount
+//    {
+//       double getDiscount(double TotalSales);
+//    }
+
+
+//    interface IDatabase
+//    {
+//        void Add();
+//    }
+//}
