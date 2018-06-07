b569a5fe2c19dbd6baa6cadb0f8ea30571353de1
Vinay Kumar Gandra <v-vinga@microsoft.com>
2018-06-07 13:47:58 +0530

    Added Single Responsibility code

--- SOLIDPrinciples/InterfaceSegregation.cs
+++ SOLIDPrinciples/InterfaceSegregation.cs
@@ -1,55 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SOLIDPrinciples
-{
-    public class InterfaceSegregation
-    {
-        static void Main(string[] args)
-        {
-            Customer cust = new Customer();
-            cust.Add();
+//namespace SOLIDPrinciples
+//{
+//    public class InterfaceSegregation
+//    {
+//        static void Main(string[] args)
+//        {
+//            Customer cust = new Customer();
+//            cust.Add();
 
-            NewCustomer newCust = new NewCustomer();
-            newCust.Read();
-            Console.ReadLine();
-        }
-    }
+//            NewCustomer newCust = new NewCustomer();
+//            newCust.Read();
+//            Console.ReadLine();
+//        }
+//    }
 
-    interface IDatabase
-    {
-        void Add(); // old client are happy with these.
-        //void Read(); // requirement from new clients.
-    }
-    interface IDatabaseV1 : IDatabase 
-    {
-        void Read();// Added for new clients.
-    }
-    class Customer : IDatabase
-    {
-        public virtual void Add()
-        {
-            Console.WriteLine("Added for customer");
-        }
+//    interface IDatabase
+//    {
+//        void Add(); // old client are happy with these.
+//        //void Read(); // requirement from new clients.
+//    }
+//    interface IDatabaseV1 : IDatabase 
+//    {
+//        void Read();// Added for new clients.
+//    }
+//    class Customer : IDatabase
+//    {
+//        public virtual void Add()
+//        {
+//            Console.WriteLine("Added for customer");
+//        }
 
-        public void Read()
-        {
-            Console.WriteLine("Read for new Customer");
-        }
-    }
-    class NewCustomer : IDatabase, IDatabaseV1
-    {
-        public virtual void Add()
-        {
-            Console.WriteLine("Added for customer");
-        }
+//        public void Read()
+//        {
+//            Console.WriteLine("Read for new Customer");
+//        }
+//    }
+//    class NewCustomer : IDatabase, IDatabaseV1
+//    {
+//        public virtual void Add()
+//        {
+//            Console.WriteLine("Added for customer");
+//        }
 
-        public void Read()
-        {
-            Console.WriteLine("Read for new Customer");
-        }
-    }
-}
+//        public void Read()
+//        {
+//            Console.WriteLine("Read for new Customer");
+//        }
+//    }
+//}
---
+++ SOLIDPrinciples/SingleResponsibility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples
+{
+    public class SingleResponsibility
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+    class Customer
+    {
+        private FileLogger obj = new FileLogger();
+        public void Add()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                //System.IO.File.WriteAllText(@"c:\Error.txt", ex.ToString());// do not do this
+                obj.Handle(ex.ToString()); //do this
+            }
+        }
+    }
+    class FileLogger
+    {
+        public void Handle(string error)
+        {
+            System.IO.File.WriteAllText(@"c:\Error.txt", error);
+        }
+    }
+}
